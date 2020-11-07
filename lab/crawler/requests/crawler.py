d9f4833a3f61d36f34f3b133616a2e2000a642d9
Alex Gheorghiu <alex@scriptoid.com>
2020-11-07 13:26:04 +0200

    Added a limit to number of links

--- lab/crawler/requests/crawler.py
+++ lab/crawler/requests/crawler.py
@@ -14,6 +14,7 @@ from bs4 import BeautifulSoup
 
 # domain = 'localhost:7000'
 domain = 'http://scriptod.com'
+# max_links = 0
 
 class Link:
 	def __init__(self, absolute_url, url):
@@ -37,7 +38,8 @@ def report(filename, visited):
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--domain', type=str, default=domain, help='Domain to crawl')
+parser.add_argument('--domain', type=str, default=domain, help='Domain to crawl', required=True)
+parser.add_argument('--max-links', type=int, default=0, help='Maximum no. of links to index')
 args = parser.parse_args()
 
 if args.domain:
@@ -46,61 +48,74 @@ else:
 	print('No domain passed, using %s.' % domain)
 	print('Read usage details in file header for more information on passing arguments.')
 
+if args.max_links:
+	max_links = args.max_links
+
 theURL = 'http://' + domain
 domain_regex = re.compile(domain)
 
-visited = [] 
-external_links = []
-l = Link(theURL,theURL)
-to_visit = [l]
-total_time = 0
-while len(to_visit) > 0:
-	# Get first link
-	current_link = to_visit.pop(0)
-	print("Visiting: %s" % current_link.absolute_url)
+class Crawler:
+	def __init__(self, initialLink):
+		self.initialLink = initialLink
+		self.visited = []
+		self.external_links = []
 
-	# Try to follow it
-	t1 = time.time()
+	def crawl(self):
+		l = Link(theURL,theURL)
+		self.to_visit = [l]
+		while len(self.to_visit) > 0 and (max_links > 0 and max_links > len(self.visited)):
+			# Get first link
+			current_link = self.to_visit.pop(0)
+			print("Visiting: %s" % current_link.absolute_url)
 
-	try:
-		r = requests.get(current_link.absolute_url)
-	except requests.ConnectionError as con_err:
-		#TODO: mark it as visit (but broken)
-		visited.append(current_link)
-		continue
-	except requests.exceptions.InvalidSchema:
-		#TODO: mark it as visit (but broken)
-		visited.append(current_link)
-		continue
+			try:
+				r = requests.get(current_link.absolute_url)
+			except requests.ConnectionError as con_err:
+				#TODO: mark it as visit (but broken)
+				self.visited.append(current_link)
+				continue
+			except requests.exceptions.InvalidSchema:
+				#TODO: mark it as visit (but broken)
+				self.visited.append(current_link)
+				continue
 
-	if r.status_code == 200:
-		soup = BeautifulSoup(r.content, "html.parser")
+			if r.status_code == 200:
+				soup = BeautifulSoup(r.content, "html.parser")
 
-		for link in soup.find_all("a"):
-			#TODO: descent into link.contents (it can be an image) and gather all text
-			if 'href' in link.attrs:
-				href = link.attrs['href']
-				print("[%s] -> %s" % (link.contents, href))
-				if re.search(domain_regex, href): # internal link
-					if (href in [l.absolute_url for l in visited] or href in [l.absolute_url for l in to_visit]) :
+				for link in soup.find_all("a"):
+					#TODO: descent into link.contents (it can be an image) and gather all text
+					if 'href' in link.attrs:
+						href = link.attrs['href']
+						print("[%s] -> %s" % (link.contents, href))
+						if re.search(domain_regex, href): # internal link
+							if (href in [l.absolute_url for l in self.visited] or href in [l.absolute_url for l in self.to_visit]) :
+								pass
+							else:
+								print("\t[%s]Plan to visit" % link.absolute_url)
+								self.to_visit.append(Link(href, to_absolute_url(current_link.absolute_url, href)))
+						else: #external link
+							if not (href in [l.absolute_url for l in self.external_links] ):
+								self.external_links.append(Link(href,to_absolute_url(current_link.absolute_url, href)))
+					elif 'name' in link.attrs:
+						# Just anchor
 						pass
-					else:
-						print("\t[%s]Plan to visit" % link.absolute_url)
-						to_visit.append(Link(href, to_absolute_url(current_link.absolute_url, href)))
-				else: #external link
-					if not (href in [l.absolute_url for l in external_links] ):
-						external_links.append(Link(href,to_absolute_url(current_link.absolute_url, href)))
-			elif 'name' in link.attrs:
-				# Just anchor
-				pass
-	else:
-		#TODO: mark it as broken
-		print("%s-->%d", current_link.absolute_url, r.status_code)
+			else:
+				#TODO: mark it as broken
+				print("%s-->%d", current_link.absolute_url, r.status_code)
 
-	visited.append(current_link)
+			self.visited.append(current_link)
 
-	print("Visited: %d To visit: %d" % (len(visited), len(to_visit)))
+			print("Visited: %d To visit: %d" % (len(self.visited), len(self.to_visit)))
 
-print("Total internal links visited: %d in: %ds" % (len(visited), total_time))
-print("Total external links: %d" % len(external_links))
-report('./crawl-requests-report.log', visited)
+
+if __name__ == "__main__":
+	crawler = Crawler(theURL)
+
+	t1 = time.time()
+	crawler.crawl()
+	t2 = time.time()
+	total_time = t2 - t1
+
+	print("Total internal links visited: %d in: %ds" % (len(crawler.visited), total_time))
+	print("Total external links: %d" % len(crawler.external_links))
+	report('./crawl-requests-report.log', crawler.visited)
