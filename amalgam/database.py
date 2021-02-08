80d1425d86d4f5254fcad104dc05e69312786923
Alex Gheorghiu <alex@scriptoid.com>
2021-02-08 13:09:03 +0200

    In progress - move away from Flask-Alchemy

--- amalgam/database.py
+++ amalgam/database.py
@@ -1,3 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+Base = declarative_base()
+
+
+engine = create_engine('sqlite:///foo.db')
+session_factory = sessionmaker(bind=engine)
