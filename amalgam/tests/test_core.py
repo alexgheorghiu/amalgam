51d7209ea46ba58cabd3bb85bac6b950603b0b6d
Alex <alex@scriptoid.com>
2021-04-04 23:08:58 +0300

    First steps toward using Core instead or ORM

--- amalgam/tests/test_core.py
+++ amalgam/tests/test_core.py
@@ -8,7 +8,7 @@ from threading import Thread, current_thread
 import uuid
 
 from sqlalchemy.sql import select
-from sqlalchemy import MetaData, Table, Column, Integer, String, insert, delete
+from sqlalchemy import MetaData, Table, Column, Integer, String, insert, delete, update
 from sqlalchemy import create_engine
 
 from amalgam.database import session_factory, engine
@@ -21,6 +21,12 @@ users = Table('users', metadata,
               Column('name', String(100), nullable=True),
               Column('email', String(100), nullable=True, unique=True),
               Column('password', String(100), nullable=True))
+
+class User:
+    def load_from_rs(self, rs):
+        pass
+
+    
 #
 # metadata.create_all(engine)
 
@@ -39,13 +45,37 @@ def add_user(parent_id, job_id):
     conn.close()
     return result.inserted_primary_key
 
+# ============================================================================================================
 def get_user_all():
     conn = engine.connect()
     s = select([users])
     rp = conn.execute(s)
     results = rp.fetchall()
     conn.close()
-    return results
+    return [dict(result) for result in results]
+
+def user_get_by_id(user_id):
+    conn = engine.connect()
+    s = select([users]).where(users.c.id == user_id)
+    rp = conn.execute(s)
+    result = rp.first()
+    conn.close()
+    return dict(result)
+
+def user_create(user):
+    conn = engine.connect()
+    # Code?
+    ins = insert(users).values(user)
+    rp = conn.execute(ins)
+    conn.close()
+
+def user_update(user):
+    conn = engine.connect()
+    # s = select([users]).where(users.c.id == user_id)
+    # rp = conn.execute(s)
+    # result = rp.first()
+    conn.close()
+# ============================================================================================================    
 
         
 reset_users()
@@ -59,12 +89,16 @@ def single_job(job_id, parent_id):
     
     print("\nSingle Job is: {} -> {}".format(parent_id, job_id))
     
-    add_user(parent_id, job_id)
+    user_id = add_user(parent_id, job_id)
     print(engine.pool.status())
 
-    users_res = get_user_all()
-    print(users_res)
+    user = user_get_by_id(user_id)
+    print(user)
+    print(type(user['id']))
 
+    user['email'] = user['email'] + ' la la la'
+    user_update(user)
+ 
     print("\nSingle Job: {} -> {} done".format(parent_id, job_id))
     sleep(1)
     
@@ -72,7 +106,7 @@ def single_job(job_id, parent_id):
 
 def composite_job(job_id):
     print("\nComposite Job is {}".format(job_id))
-    NO = 10
+    NO = 3
     workers = []
 
     # Create worker threads
@@ -90,7 +124,7 @@ def composite_job(job_id):
     print("\nComposite Job {} done".format(job_id))
 
 
-CJ_NO = 10
+CJ_NO = 3
 cj_workers = []
 
 # Create worker threads
