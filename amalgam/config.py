300c7996cc94a62266879de24f8f2a1f255e4db4
Alex <alex@scriptoid.com>
2021-04-22 12:41:31 +0300

    Fix identation

--- amalgam/config.py
+++ amalgam/config.py
@@ -2,8 +2,8 @@ import logging, os
 
 def setup_logging():
     """Setup the logging for whole application"""
-	stream_handler = logging.StreamHandler() # STDOUT loggin
-	file_handler = logging.FileHandler(filename=os.environ.get("AMALGAM_LOG_FILE", 'amalgam.log'))
-	logging.basicConfig(level=os.environ.get("AMALGAM_LOG_LEVEL", "INFO"), 
-		format='%(asctime)s %(name)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
-		handlers=[stream_handler, file_handler])
+    stream_handler = logging.StreamHandler() # STDOUT loggin
+    file_handler = logging.FileHandler(filename=os.environ.get("AMALGAM_LOG_FILE", 'amalgam.log'))
+    logging.basicConfig(level=os.environ.get("AMALGAM_LOG_LEVEL", "INFO"), 
+    format='%(asctime)s %(name)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+    handlers=[stream_handler, file_handler])
